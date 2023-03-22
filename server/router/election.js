89760f67494d00c456663e93189ee548272c551d
Eashwar MP <eashwarmp1998@gmail.com>
2023-03-22 13:44:09 -0400

    Removed comments

--- server/router/election.js
+++ server/router/election.js
@@ -4,19 +4,6 @@ const Election = require('../models/electionModel')
 
 const router = new express.Router();
 
-//An endpoint to fetch the secret ballot of a profile
-// router.post('/getSecretBallot', async (req,res) => {
-//     try {
-//         const ballotDoc = await Voter.findOne(req.body);
-//         console.log(ballotDoc)
-//         let secretBallot = ballotDoc.secretBallot ? ballotDoc.secretBallot : null;
-//         return secretBallot !== null ?  res.json(secretBallot) :  res.json("Unable to fetch the ballot")
-//     } catch(err) {
-//         console.log(`Exception caught --------> ${err}`)
-//         return res.status(500).send(err);
-//     }
-// })
-
 //Endpoint to create an election
     router.post('/createElection', async (req,res) => {
         try {
